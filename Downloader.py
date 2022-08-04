6e46dbeeea48dbaf0c3c0fb2a34607083d17f6cb
Ethan Bitnun <ebitnun@uwaterloo.ca>
2022-08-04 19:06:25 -0400

    Updated comments, updated output spacing for looks

--- Downloader.py
+++ Downloader.py
@@ -2,34 +2,37 @@ from pytube import YouTube
 from pydub import AudioSegment
 import os
 
-url = input("Paste in your youtube url: ")
+# User inputs youtube url
+url = input(" Paste in your youtube url: ")
 video = YouTube(url)
 
 print(video.title)
 
-print("Downloading Highest Quality Audio...")
+print(" Downloading Highest Quality Audio...")
 
+# Finds and returns the itag corresponding to the best audio 
+# quality file from the youtube API
 def get_best_audio():
     previous_quality = int(0)
 
+    # Iterates through every audio-only quality option and finds the best
     for stream in video.streams.filter(only_audio = True):
+
+        # Removes the Kbps from the end of the string and casts to int
         current_quality = int( stream.abr[0:(len(stream.abr)-4)])
-        # print ( int( stream.abr[0:(len(stream.abr)-4)] ))
-        # print (isinstance( current_quality, int))
-        # print (isinstance( previous_quality, int))
         
+        # If the current quality is better, set this to the new best audio
         if ( current_quality > previous_quality):
             best_audio_tag = stream.itag
             previous_quality = current_quality
-            # print (best_audio_tag)
     
     return best_audio_tag
 
 
-#set stream resolution
+# Set the stream resolution to the best audio quality
 audio = video.streams.get_by_itag(get_best_audio())
 
-#Download video
+# Download it
 audio.download()
 
 print (" ")
@@ -41,6 +44,7 @@ print (" Converting to mp3...")
 audio_file_name = audio.title + ".webm"
 converted_audio_file_name = audio.title + ".mp3"
 
+# Convert the webm file to mp3
 mp3_audio = AudioSegment.from_file(audio_file_name)#, format="webm")
 mp3_audio.export(converted_audio_file_name, format="mp3")
 
